3900e0dfd0c4488c92358f1c40c6372ba87d2942
Sameer Kumar <er.makhija09@gmail.com>
2017-10-18 08:03:22 -0700

    Array - each and Array - some

--- ArrayES5Implementation.js
+++ ArrayES5Implementation.js
@@ -1,4 +1,13 @@
-//Reference Link :-  https://gist.github.com/alexhawkins/28aaf610a3e76d8b8264
+/*
+Reference Link :-  https://gist.github.com/alexhawkins/28aaf610a3e76d8b8264
+  Array - forEach
+  Array - map
+  Array - filter
+  Array - every
+  Array - some
+  Array - reduce
+*/
+
 
 //Array - ForEach 
 Array.prototype.myForEach = function(callback) {
@@ -52,4 +61,47 @@ Array.prototype.myFilter = function (callback, context) {
     return value > 10;
   });
   console.log(`Filtered Array > 10 = ${JSON.stringify(filteredArray)}`);
+})();
+
+//Array - every
+const allNumberAbove10 = [11,12,25,15];
+Array.prototype.myEvery = function (callback, context){
+  for(let i = 0; i < this.length; i++) {
+    if(!callback.call(context, this[i], i, this)){
+      return false;
+    }
+  }
+  return true;
+};
+
+(function(){
+  const numberArrayResult = numberArray.myEvery(function(value){
+    return value > 10;
+  });
+  const resultAbove10 = allNumberAbove10.myEvery(function(value){
+    return value > 10;
+  });
+  console.log(`Every | allNumberAbove10 = All values > 10 =  ${resultAbove10}`);
+  console.log(`Every | numberArrayResult = All values > 10 =  ${numberArrayResult}`);
+})();
+
+//Array - some
+Array.prototype.mySome = function (callback, context){
+  for(let i = 0; i < this.length; i++) {
+    if(callback.call(context, this[i], i, this)){
+      return true;
+    }
+  }
+  return false;
+};
+
+(function(){
+  const numberArrayResult = numberArray.mySome(function(value){
+    return value > 10;
+  });
+  const resultAbove10 = allNumberAbove10.mySome(function(value){
+    return value > 10;
+  });
+  console.log(`Some | allNumberAbove10 = All values > 10 =  ${resultAbove10}`);
+  console.log(`Some | numberArrayResult = All values > 10 =  ${numberArrayResult}`);
 })();
